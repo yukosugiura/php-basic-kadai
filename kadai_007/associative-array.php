b766b003c1a1b830bbb5b8698ec0d28aca3bc34d
yukosugiura <157888777+yukosugiura@users.noreply.github.com>
2024-04-09 09:57:23 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_007</title>
+ </head>
+ 
+ <body>
+     <p>
+      <?php
+      $goods_data=['name'=>'onion','price'=>200,'weight'=>160];
+      print_r($goods_data);
+      ?>
+      </p>
+</body>
+</html>
